8328ef5b15070367249c56fe1bac191b351164d9
cher1shRXD <taewoo080401@gmail.com>
2024-07-15 11:57:45 +0900

    fix::scroll-snap

--- src/pages/shorts/style.ts
+++ src/pages/shorts/style.ts
@@ -7,7 +7,7 @@ export const Cotainer = styled.div`
   height: calc(100vh - 10rem);
   overflow-y: scroll;
   scroll-snap-type: y mandatory;
-  scroll-padding: 10rem 0;
+  scroll-margin: 10rem 0;
   -ms-overflow-style: none;
   scrollbar-width: none;
   &::-webkit-scrollbar {
