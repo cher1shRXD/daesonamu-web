96da4a68bddca5f73224ea10e7a5bd04641b2ca7
cher1shRXD <taewoo080401@gmail.com>
2024-07-16 10:33:31 +0900

    fix::like button bug

--- src/components/LikeBtn/index.tsx
+++ src/components/LikeBtn/index.tsx
@@ -33,7 +33,7 @@ const LikeBtn = (props:{boardId:number,likes:number}) => {
 
   const likeAction = async () => {
     setLoading(true);
-    await instance.delete(`/likes/${props.boardId}`)
+    await instance.post(`/likes/${props.boardId}`)
     .then((res)=>{
       setLikeCount(res.data);
       setLikeStatus(true);
@@ -53,7 +53,7 @@ const LikeBtn = (props:{boardId:number,likes:number}) => {
 
   const dislikeAction = async () => {
     setLoading(true);
-    await instance.post(`/likes/${props.boardId}`)
+    await instance.delete(`/likes/${props.boardId}`)
     .then((res) => {
       setLikeCount(res.data);
       setLikeStatus(false);
